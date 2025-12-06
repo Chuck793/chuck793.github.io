7374c2008cea7b92d9127dbbc791df411bae6fa6
Chuck793 <charlkadlec@gmail.com>
2025-12-06 09:20:18 +0000

    added js

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+let now = new Date();
+console.log(now);
+
+let time = now.getHours();
+
+if (time < 10){
+    alert("good morning");
+}   else if (time < 12){
+    alert("good noon");
+}   else {
+    alert("good afterenoon");
+}
+
+
+
+
+
+
+
+
+
